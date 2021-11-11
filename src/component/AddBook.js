7e5a0be45b01e330e095d0645cb73bbb392cb70b
mahbub <mahbub21alam2017@gmail.com>
2021-11-12 01:02:22 +0600

    Connect bookstore to api

--- src/component/AddBook.js
+++ src/component/AddBook.js
@@ -6,19 +6,19 @@ import { addBook } from '../redux/books/books';
 const InsertBook = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const submitBookToStore = (e) => {
     e.preventDefault();
     const newBook = {
       id: uuidv4(),
       title,
-      author,
+      category,
     };
 
     dispatch(addBook(newBook));
     setTitle('');
-    setAuthor('');
+    setCategory('');
   };
 
   return (
@@ -26,7 +26,7 @@ const InsertBook = () => {
       <div>
         <h3>Add Book</h3>
         <input placeholder="Add Title" value={title} type="text" onChange={(e) => setTitle(e.target.value)} />
-        <input placeholder="Add Author" value={author} type="text" onChange={(e) => setAuthor(e.target.value)} />
+        <input className="add-book" placeholder="Add Category" value={category} type="text" onChange={(e) => setCategory(e.target.value)} />
         <button type="submit"> Add Book</button>
       </div>
     </form>
--- src/component/BookList.js
+++ src/component/BookList.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import AddBook from './AddBook';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getBooks } from '../redux/books/books';
+import InsertBook from './AddBook';
 import Book from './Book';
 
 const BookList = () => {
-  const books = useSelector((state) => state.booksReducer);
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <ul>
       {books.map((book) => (
         <Book key={book.id} book={book} />
       ))}
-      <AddBook />
+      <InsertBook />
     </ul>
   );
 };
