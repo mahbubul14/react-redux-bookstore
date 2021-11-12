249b19cfd3fcf679266c17905b1857397f3513e1
mahbub <mahbub21alam2017@gmail.com>
2021-11-12 16:05:41 +0600

    Add icon and update style of booksttore

--- src/component/Book.js
+++ src/component/Book.js
@@ -12,15 +12,25 @@ const Book = ({ book }) => {
 
   return (
     <div>
-      <li className="item">
-        <div className="column flex gray">
-          <p>{book.category}</p>
+      <li className="flex book-list">
+        <div className="column flex item">
+          <p className="gray">{book.category}</p>
           <p className="book-title">{book.title}</p>
-          <div className="flex">
+          <div className="btn-container flex">
             <button className="gap-r btn" type="button">Comments</button>
             <button onClick={() => removeBookHandler(book.item_id)} type="button" className="rmBtn gap-r btn">Remove</button>
             <button className="gap-r btn" type="button">Edit</button>
           </div>
+        </div>
+        <div className="oval" />
+        <div>
+          <span className="percent">64%</span>
+          <p className="completed">Completed</p>
+        </div>
+        <div className="list-r-com">
+          <p className="gray">CURRENT CHAPTER</p>
+          <p>CHAPTER 17</p>
+          <button type="button" className="update-btn">UPDATE PROGRESS</button>
         </div>
       </li>
     </div>
